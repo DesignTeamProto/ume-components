8919e29b5be3fe9497ce085c1e17e3958ea4b974
petermooibroek <petermooibroek@gmail.com>
2018-11-02 13:47:30 +0100

    Updated checkbox

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Checkbox as BaseCheckbox } from '@material/react-checkbox/dist'; // /index.js is implied
+import BaseCheckbox from '@material/react-checkbox/dist'; // /index.js is implied
 
 const StyledCheckbox = styled(BaseCheckbox)`
   border-radius: 2px;
 `;
 
 const Checkbox = (props, { qaTag, accessibilityLabel }) => {
-  const dataProps = { 'data-qa': qaTag, 'data-foo': 'Checkbox' };
+  const dataProps = { 'data-qa': qaTag };
   const ariaProps = { 'aria-label': accessibilityLabel };
   return (
     <StyledCheckbox {...props} {...dataProps} {...ariaProps} />
