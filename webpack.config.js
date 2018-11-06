c4e8f469872b350e814dfcd261bffa4d9c379532
Ronald Zwiers <ronald.zwiers@embracesbs.com>
2018-11-06 17:39:30 +0100

    Test compile

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,35 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  entry: {
-    Button: './src/components/Button/index.js',
-    ButtonGroup: './src/components/ButtonGroup/index.js',
-    Checkbox: './src/components/Checkbox/index.js',
-  },
+  entry: './src/app.js',
   output: {
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'lib/[name].js',
-    libraryTarget: 'commonjs2', // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
   },
   module: {
     rules: [
       {
+        test: /\.s?[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { url: false, sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ],
+      },
+      {
         test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        exclude: /(node_modules|build)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react',
-            ],
-          },
-        },
+        exclude: /node_modules/,
+        use: 'babel-loader',
       },
     ],
   },
-  externals: {
-    react: 'commonjs react', // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
-  },
+  devtool: 'source-map',
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+  mode: devMode ? 'development' : 'production',
 };
