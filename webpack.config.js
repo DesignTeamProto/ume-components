bebdc4833abcf290682a8dae0cdecfbacb1af798
Ronald Zwiers <ronald.zwiers@embracesbs.com>
2018-11-06 17:50:45 +0100

    Uppercased App.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/App.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
