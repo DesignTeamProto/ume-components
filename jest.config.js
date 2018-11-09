31f3a2262ab4e58beb340a9e6a12af8899e58dc3
petermooibroek <petermooibroek@gmail.com>
2018-11-09 10:53:49 +0100

    Start script opens Styleguidist

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     'jest-junit',
   ],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/assetsTransformer.js',
-    '\\.(css|scss|less)$': '<rootDir>/assetsTransformer.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/util/assetsTransformer.js',
+    '\\.(css|scss|less)$': '<rootDir>/util/assetsTransformer.js',
   },
 };
